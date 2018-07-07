06f42dd5ce5c3901e5c66d9ed8802c944331f63f
Aaron <pioneervon@gmail.com>
2018-07-07 23:20:05 +0800

    Anko.
    Signed-off-by: Aaron <pioneervon@gmail.com>

--- app/src/main/java/com/pioneer/aaron/dolly/MainActivity.kt
+++ app/src/main/java/com/pioneer/aaron/dolly/MainActivity.kt
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
 import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
 import com.pioneer.aaron.dolly.fork.DataBaseOperator
 import com.pioneer.aaron.dolly.fork.calllog.ForkCallLogActivity
 import com.pioneer.aaron.dolly.fork.calllog.ForkCallLogData
@@ -22,113 +23,9 @@ class MainActivity : AppCompatActivity(), IMainContract.View {
 
     private lateinit var mPresenter: IMainContract.Presenter
 
-    private var mOnClickListener = { it: View ->
-        when (it) {
-            mForkCallLogButton -> {
-                val forkCallLogIntent = Intent(this@MainActivity, ForkCallLogActivity::class.java)
-                startActivity(forkCallLogIntent)
-            }
-            mForkContactButton -> {
-                val forkContactIntent = Intent(this@MainActivity, ForkContactsActivity::class.java)
-                startActivity(forkContactIntent)
-            }
-            mForkVvmButton -> {
-                val forkVvmIntent = Intent(this@MainActivity, ForkVvmActivity::class.java)
-                startActivity(forkVvmIntent)
-            }
-            else -> {
-            }
-        }
-    }
-
-    private var mOnLongClickListener: View.OnLongClickListener = View.OnLongClickListener {
-        val columnExists = DataBaseOperator.getInstance(this@MainActivity).columnsExists
-
-        if (columnExists.get(ForkCallLogData.SUBJECT)!!
-                && columnExists[ForkCallLogData.POST_CALL_TEXT]!!
-                && columnExists[ForkCallLogData.IS_PRIMARY]!!) {
-            mPresenter.vibrate()
-            mPresenter.forkRCS(this@MainActivity)
-        }
-        true
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        scrollView {
-            padding = dip(resources.getDimensionPixelOffset(R.dimen.activity_horizontal_margin))
-            verticalLayout {
-                mForkCallLogButton = linearLayout {
-                    isClickable = true
-                    setBackgroundResource(R.drawable.ripple_btn)
-                    gravity = Gravity.CENTER
-                    setPadding(0, dimen(R.dimen.fork_btn_padding),
-                            0, dimen(R.dimen.fork_btn_padding))
-
-                    imageView(R.drawable.ic_call_log)
-                    textView(R.string.fork_calllog_btn) {
-                        textColor = getColor(android.R.color.black)
-                        typeface = Typeface.DEFAULT_BOLD
-                    }.lparams {
-                        marginStart = dimen(R.dimen.fork_image_btn)
-                        gravity = Gravity.CENTER
-                    }
-
-                    setOnClickListener(mOnClickListener)
-                    setOnLongClickListener(mOnLongClickListener)
-                }.lparams(width = matchParent) {
-                    topMargin = dimen(R.dimen.fork_btn_margin)
-                }
-
-                mForkContactButton = linearLayout {
-                    isClickable = true
-                    setBackgroundResource(R.drawable.ripple_btn)
-                    gravity = Gravity.CENTER
-                    setPadding(0, dimen(R.dimen.fork_btn_padding),
-                            0, dimen(R.dimen.fork_btn_padding))
-
-                    imageView(R.drawable.ic_contact)
-                    textView(R.string.fork_contact_btn) {
-                        textColor = getColor(android.R.color.black)
-                        typeface = Typeface.DEFAULT_BOLD
-                    }.lparams {
-                        marginStart = dimen(R.dimen.fork_image_btn)
-                        gravity = Gravity.CENTER
-                    }
-
-                    setOnClickListener(mOnClickListener)
-                }.lparams(width = matchParent) {
-                    topMargin = dimen(R.dimen.fork_btn_margin)
-                }
-
-                mForkVvmButton = linearLayout {
-                    isClickable = true
-                    setBackgroundResource(R.drawable.ripple_btn)
-                    gravity = Gravity.CENTER
-                    setPadding(0, dimen(R.dimen.fork_btn_padding),
-                            0, dimen(R.dimen.fork_btn_padding))
-
-                    imageView(R.drawable.voicemail) {
-                        scaleType = ImageView.ScaleType.FIT_CENTER
-                    }.lparams(width = dimen(R.dimen.fork_btn_width),
-                            height = dimen(R.dimen.fork_btn_height))
-                    textView(R.string.fork_vvm_btn) {
-                        textColor = getColor(android.R.color.black)
-                        typeface = Typeface.DEFAULT_BOLD
-                    }.lparams {
-                        marginStart = dimen(R.dimen.fork_image_btn)
-                        gravity = Gravity.CENTER
-                    }
-
-                    setOnClickListener(mOnClickListener)
-                }.lparams(width = matchParent) {
-                    topMargin = dimen(R.dimen.fork_btn_margin)
-                }
-
-
-            }
-        }
-
+        MainActivityUI().setContentView(this)
         mPresenter = MainPresenter(this)
     }
 
@@ -141,5 +38,89 @@ class MainActivity : AppCompatActivity(), IMainContract.View {
     override fun onDestroy() {
         mPresenter.onDestroy(this)
         super.onDestroy()
+    }
+
+    inner class MainActivityUI : AnkoComponent<MainActivity> {
+        override fun createView(ui: AnkoContext<MainActivity>): View =
+                with(ui) {
+                    scrollView {
+                        padding = dip(resources.getDimensionPixelOffset(R.dimen.activity_horizontal_margin))
+                        verticalLayout {
+                            mForkCallLogButton = linearLayout {
+                                imageView(R.drawable.ic_call_log)
+                                textView(R.string.fork_calllog_btn) {
+                                    textColor = getColor(android.R.color.black)
+                                    typeface = Typeface.DEFAULT_BOLD
+                                }.lparams {
+                                    marginStart = dimen(R.dimen.fork_image_btn)
+                                    gravity = Gravity.CENTER
+                                }
+                                setOnClickListener {
+                                    startActivity(Intent(this@MainActivity, ForkCallLogActivity::class.java))
+                                }
+                                setOnLongClickListener {
+                                    val columnExists = DataBaseOperator.getInstance(this@MainActivity).columnsExists
+
+                                    if (columnExists[ForkCallLogData.SUBJECT]!!
+                                            && columnExists[ForkCallLogData.POST_CALL_TEXT]!!
+                                            && columnExists[ForkCallLogData.IS_PRIMARY]!!) {
+                                        mPresenter.vibrate()
+                                        mPresenter.forkRCS(this@MainActivity)
+                                    }
+                                    true
+                                }
+                            }
+
+                            mForkContactButton = linearLayout {
+                                imageView(R.drawable.ic_contact)
+                                textView(R.string.fork_contact_btn) {
+                                    textColor = getColor(android.R.color.black)
+                                    typeface = Typeface.DEFAULT_BOLD
+                                }.lparams {
+                                    marginStart = dimen(R.dimen.fork_image_btn)
+                                    gravity = Gravity.CENTER
+                                }
+
+                                setOnClickListener {
+                                    startActivity(Intent(this@MainActivity, ForkContactsActivity::class.java))
+                                }
+                            }.lparams(width = matchParent) {
+                                topMargin = dimen(R.dimen.fork_btn_margin)
+                            }
+
+                            mForkVvmButton = linearLayout {
+                                imageView(R.drawable.voicemail) {
+                                    scaleType = ImageView.ScaleType.FIT_CENTER
+                                }.lparams(width = dimen(R.dimen.fork_btn_width),
+                                        height = dimen(R.dimen.fork_btn_height))
+                                textView(R.string.fork_vvm_btn) {
+                                    textColor = getColor(android.R.color.black)
+                                    typeface = Typeface.DEFAULT_BOLD
+                                }.lparams {
+                                    marginStart = dimen(R.dimen.fork_image_btn)
+                                    gravity = Gravity.CENTER
+                                }
+
+                                setOnClickListener {
+                                    startActivity(Intent(this@MainActivity, ForkVvmActivity::class.java))
+                                }
+                            }.lparams(width = matchParent) {
+                                topMargin = dimen(R.dimen.fork_btn_margin)
+                            }
+                        }.applyRecursively { view ->
+                            when (view) {
+                                mForkCallLogButton, mForkContactButton, mForkVvmButton -> {
+                                    view.isClickable = true
+                                    view.setBackgroundResource(R.drawable.ripple_btn)
+                                    view.setPadding(0, dimen(R.dimen.fork_btn_padding),
+                                            0, dimen(R.dimen.fork_btn_padding))
+                                    if (view is LinearLayout) {
+                                        view.gravity = Gravity.CENTER
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
     }
 }
