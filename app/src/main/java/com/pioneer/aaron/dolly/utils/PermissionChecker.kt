e06c4db4bcc9b03d738dc81c78439618092d4e56
Aaron <pioneervon@gmail.com>
2018-07-07 17:53:07 +0800

    Bugfix: add voicemail crash.
    Signed-off-by: Aaron <pioneervon@gmail.com>

--- app/src/main/java/com/pioneer/aaron/dolly/utils/PermissionChecker.kt
+++ app/src/main/java/com/pioneer/aaron/dolly/utils/PermissionChecker.kt
@@ -14,10 +14,12 @@ object PermissionChecker {
 
     const val PERMISSION_REQUEST_CODE = 100
 
-    private val PERMISSIONS_NEEDED = arrayOf(Manifest.permission.READ_CALL_LOG, Manifest.permission.WRITE_CALL_LOG, Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS)
+    private val PERMISSIONS_NEEDED = arrayOf(Manifest.permission.ADD_VOICEMAIL, Manifest.permission.READ_CALL_LOG,
+            Manifest.permission.WRITE_CALL_LOG, Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS)
 
     fun checkPermissions(activity: Activity): Boolean {
-        return if (ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_CALL_LOG) != PackageManager.PERMISSION_GRANTED
+        return if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ADD_VOICEMAIL) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_CALL_LOG) != PackageManager.PERMISSION_GRANTED
                 || ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(activity, PERMISSIONS_NEEDED, PERMISSION_REQUEST_CODE)
             false
